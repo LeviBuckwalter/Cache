8f81219f4a6528bca9fd954ad49ca765c001fd39
Levi <levibuck4@gmail.com>
2024-06-03 08:03:48 -0400

    (a) delete startCaching (b) add requestWrite (c) add store and retrieve (d) reorder functions (e) add documentation
    a/b: I was thinking about it and I just didn't like having that caching loop going in the background. It was stressing me out. It was like "will it keep looping after I close the program?" So I came up with this different way to do it.
    c: honestly this kinda goes along with a/b; it's there to make things simpler for the end user.
    d: just seemed like a better order.
    e: I figured it'd be good to add documentation.

--- functions.js
+++ functions.js
@@ -1,22 +1,50 @@
 import fs from "node:fs/promises"
 import { globals } from "./globals.js"
 
-export async function writeCache() {
+async function writeCache() {
+    /*
+    This function turns globals.cacheObj into a JSON string, and then puts that into cache.txt. It deletes whatever was in cache.txt and replaces it.
+    */
     await fs.writeFile("./cache.txt", JSON.stringify(globals.cacheObj))
 }
 export async function readCache() {
+    /*
+    This function reads cache.txt and copies it into globals.cacheObj. It's like "load in the saved data from my previous session."
+    It should probably only ever be run once per session, upon startup.
+    */
     globals.cacheObj = JSON.parse(await fs.readFile("./cache.txt"))
 }
 
-export function startCaching(milliseconds) {
-    if (milliseconds === undefined) {
-        milliseconds = 250
-    }
-    function cache() {
-        if (!globals.cacheUpToDate) {
+function requestWrite() {
+    /*
+    The reason for this function is that I don't want to be writing to the cache a hundred times per second. So, instead, whenever I want to write to the cache, I call this function, and it will either start a pending action to write to cache, or it will do nothing if it sees that there is already a pending action to write to the cache.
+    */
+
+    if (globals.writePending) {
+        //do nothing
+    } else {
+        globals.writePending = true
+        function f() {
             writeCache()
-            globals.cacheUpToDate = true
+            globals.writePending = false
         }
+        setTimeout(f, 250)
     }
-    setInterval(cache, milliseconds)
+}
+
+export function store(item, id) {
+    /*
+    This is a higher level function. It's for the user. Just say what you want to store and what id to store it under, and that's all you have to think about.
+    What the function does is it stores the item in globals.cacheObj under the given id, and then it requests a write to cache.txt.
+    */
+
+    globals.cacheObj[id] = item
+    requestWrite()
+}
+
+export function retrieve(id) {
+    /*
+    This function just returns the value stored in globals.cacheObj under the given id. It does not read from cache.txt.
+    */
+    return globals.cacheObj[id]
 }
