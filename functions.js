401f26b0966180926370adac809c1308f816bdd6
Levi <levibuck4@gmail.com>
2024-06-08 16:53:32 -0400

    1) reorder functions 2) add putOutHit function 3) add removeAllExpired function 4) add initializeCache function 5) change vocabulary from "id" to "key" 6) implement expiration to cache 7) add metadata to entries in cache 8) add discard function 9) add clearCache function
    1: trying to make it more readable
    2: this way items will remove themselves once they're expired
    3: this is to run at the beginning, since the putOutHit functions will have been ended after closing the program.
    4: just more user friendly to bundle a couple functions together
    6: now you have to specify a shelf life when you store something
    7: this comes in handy in managing the cache, like removing items to save space.
    9: sometimes you're gonna wanna clear the cache

--- functions.js
+++ functions.js
@@ -1,18 +1,62 @@
 import fs from "node:fs/promises"
 import { globals } from "./globals.js"
 
-async function writeCache() {
-    /*
-    This function turns globals.cacheObj into a JSON string, and then puts that into cache.txt. It deletes whatever was in cache.txt and replaces it.
-    */
-    await fs.writeFile("./cache.txt", JSON.stringify(globals.cacheObj))
+function putOutHit(cacheKey) {
+    const mark = globals.cacheObj[cacheKey]
+    if (mark.expireTS === null) {
+        //expireTS === null means it's evergreen and won't expire. It throws an error when you try to and do a setTimeout with a duration of null.
+        console.log("mark.expireTS === null")
+        return
+    } else {
+        setTimeout(
+            function () {
+                if (Date.now() >= mark.expireTS - 5) {
+                    discard(cacheKey)
+                } else {
+                    console.log("it wasn't time yet")
+                    console.log(mark.expireTS, Date.now())
+                    console.log(mark)
+                }
+            },
+            mark.expireTS - Date.now()
+        )
+    }
+
 }
+
+function removeAllExpired() {
+    /* Expired entries will be removed automatically, but when you first boot up your program there are bound to be a bunch of things that are expired, and those won't be removed automatically. So this function will go through the cache manually and remove items that are past expiration. */
+    let cacheKeys = Object.keys(globals.cacheObj)
+    for (let key of cacheKeys) {
+        const entry = globals.cacheObj[key]
+        if (entry.expireTS !== null) {
+            if (Date.now() >= entry.expireTS) {
+                discard(key)
+            } else {
+                putOutHit(key)
+            }
+        }
+    }
+}
+
 export async function readCache() {
     /*
     This function reads cache.txt and copies it into globals.cacheObj. It's like "load in the saved data from my previous session."
     It should probably only ever be run once per session, upon startup.
     */
     globals.cacheObj = JSON.parse(await fs.readFile("./cache.txt"))
+}
+
+export async function initializeCache() {
+    await readCache()
+    removeAllExpired()
+}
+
+async function writeCache() {
+    /*
+    This function turns globals.cacheObj into a JSON string, and then puts that into cache.txt. It deletes whatever was in cache.txt and replaces it.
+    */
+    await fs.writeFile("./cache.txt", JSON.stringify(globals.cacheObj))
 }
 
 function requestWrite() {
@@ -32,23 +76,47 @@ function requestWrite() {
     }
 }
 
-export function store(item, id) {
+export function store(item, key, shelfLife) {
     /*
     This is a higher level function. It's for the user. Just say what you want to store and what id to store it under, and that's all you have to think about.
     What the function does is it stores the item in globals.cacheObj under the given id, and then it requests a write to cache.txt.
     */
+    const entry = {
+        item: item,
+        conceptionTS: Date.now(),
+        expireTS: (shelfLife === null) ? null : Date.now() + shelfLife,
+    }
 
-    globals.cacheObj[id] = item
+    globals.cacheObj[key] = entry
+    if (shelfLife !== null) {
+        putOutHit(key)
+    }
     requestWrite()
 }
 
-export function retrieve(id) {
+export function retrieve(key) {
     /*
     This function just returns the value stored in globals.cacheObj under the given id. It does not read from cache.txt.
     */
-    return globals.cacheObj[id]
+    return globals.cacheObj[key].item
 }
 
-export function inCacheObj(id) {
-    return globals.cacheObj.hasOwnProperty(id)
+export function inCacheObj(key) {
+    return globals.cacheObj.hasOwnProperty(key)
+}
+
+export function discard(key) {
+    delete globals.cacheObj[key]
+    requestWrite()
+}
+
+export function clearCache() {
+    const cacheKeys = Object.keys(globals.cacheObj)
+    for (let key of cacheKeys) {
+        const entry = globals.cacheObj[key]
+        if (entry.expireTS !== null) {
+            discard(key)
+        }
+    }
+    requestWrite()
 }
