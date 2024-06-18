56b3ae466818eb3caf0c062d7d25688fe821a07e
Levi <levibuck4@gmail.com>
2024-06-18 10:09:26 -0400

    add parseAndStringify.js
    I needed to do this because regular JSON.parse gets rid of data like Sets and custom classes

---
+++ functions/parseAndStringify.js
@@ -0,0 +1,78 @@
+/*
+node terminal pastable:
+let {customStringify, customParse} = await import("./functions/parseAndStringify.js")
+let { Entry } = await import("./classes/entry.js")
+let E = new Entry("bread_thoughts", ["mmmmmmm bread", "god i love bread", "breadbreadbread"], true, 1000000)
+let obj = {undefined: undefined, null: null, NaN: NaN, Infinity: Infinity}
+let str = customStringify(obj)
+*/
+
+import { Entry } from "../classes/entry.js"
+
+export function customStringify(given) {
+    //does not preserve NaNs (become null)
+
+    function replacer(key, value) {
+        if (value && value.__type) {
+            console.log(`asked to replace something which will confuse the reviver! unable to replace objects with properties called "__type".`)
+            return undefined
+        }
+
+        if (value instanceof Set) {
+            return { __type: `Set`, contents: [...value] }
+        }
+        if (value instanceof Entry) {
+            return { __type: `Entry`, skeleton: { key: value.key, contents: value.contents, doesExpire: value.doesExpire, expireTS: value.expireTS } }
+        }
+        //This causes maximum callback error:
+        // if (isNaN(value)) {
+        //     return { __type: "NaN" }
+        // }
+        if (value === Infinity) {
+            return { __type: "Infinity" }
+        }
+        //this might not be doing anything?
+        if (value === undefined) {
+            return { __type: "undefined" }
+        }
+
+        //else:
+        return value
+    }
+
+    return JSON.stringify(given, replacer)
+}
+
+export function customParse(string) {
+    function reviver(key, value) {
+        if (value) {
+            if (value.__type === `Set`) {
+                return new Set(value.contents)
+            }
+            if (value.__type === `Entry`) {
+                let E = new Entry(value.skeleton.contents)
+                E.doesExpire = value.skeleton.doesExpire
+                if (value.skeleton.doesExpire) {
+                    E.expireTS = value.skeleton.expireTS
+                }
+
+                return E
+            }
+            //NaN doesn't work
+            // if (value.__type === `NaN`) {
+            //     return NaN
+            // }
+            if (value.__type === `Infinity`) {
+                return Infinity
+            }
+            if (value.__type === `undefined`) {
+                return undefined
+            }
+        }
+
+
+        return value
+    }
+
+    return JSON.parse(string, reviver)
+}
