3a56ae5397ec59a8ddde99a63d64ea0520e36264
Levi <levibuck4@gmail.com>
2024-06-18 10:06:42 -0400

    add parameters.js
    This was a long time comin. Better to have my magic numbers in one spot.

---
+++ parameters.js
@@ -0,0 +1,5 @@
+export const params = {
+    cacheMaxBites: 500 * 1000000,    //cache is not to exceed 500 MBs
+    ledgSetsMaxProp: 0.2,           //the maximum size of a set in ledger relative to the size of cache.entries
+    entryExtraBites: 150            //how many extra bites to add to the size of each entry to account for metadata
+}
