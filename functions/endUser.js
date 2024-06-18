1e2ccee60018ea6ac8694d9ecca35b0881758642
Levi <levibuck4@gmail.com>
2024-06-18 10:01:07 -0400

    redo endUser.js
    briefer, better organized, more focused. I also implemented a bunch of new stuff - percolated changes essentially.

---
+++ functions/endUser.js
@@ -0,0 +1,56 @@
+import { cache } from "../cache.js"
+import { Entry } from "../classes/entry.js"
+import { addToLedger, removeFromLedger, trimCache } from "./metaFunctions.js"
+
+
+export function store(key, contents, shelfLife) {
+    const expires = (shelfLife) ? true : false
+    const entry = new Entry(contents, expires, shelfLife)
+    cache.entries[key] = entry
+    addToLedger(key)
+    cache.metadata.amtEntries++
+    cache.metadata.totalBites += entry.bites
+    trimCache()
+}
+
+export function retrieve(key) {
+    //is there an entry under this key?
+    if (!cache.entries.hasOwnProperty(key)) {
+        //no:
+        return undefined
+    }
+    //yes:
+
+    //is the entry expired?
+    const entry = cache.entries[key]
+    if (entry.expired) {
+        //yes:
+        discard(key)
+        return undefined
+    }
+    //no:
+
+    //update ledger and return
+    removeFromLedger(key)
+    addToLedger(key)
+    return entry.contents
+}
+
+export function discard(key) {
+    //is there an entry under this key?
+    if (!cache.entries.hasOwnProperty(key)) {
+        console.log(`was asked to discard the entry under "${key}", but no such entry exists`)
+        return
+    }
+
+    cache.metadata.amtEntries--
+    cache.metadata.totalBites -= cache.entries[key].bites
+    removeFromLedger(key)
+    delete cache.entries[key]
+}
+
+export function keysThatStartWith(string) {
+    const regex = new RegExp(`^${string}.*`)
+    const keys = Object.keys(cache.entries)
+    return keys.filter(key => regex.test(key))
+}
