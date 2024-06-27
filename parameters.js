a4ca8a419c457c6b287a20731b0e02e9cbbf6d65
Levi <levibuck4@gmail.com>
2024-06-27 17:38:24 -0400

    switch to typescript

--- parameters.js
+++
@@ -1,5 +0,0 @@
-export const params = {
-    cacheMaxBites: 500 * 1000000,    //cache is not to exceed 500 MBs
-    ledgSetsMaxProp: 0.2,           //the maximum size of a set in ledger relative to the size of cache.entries
-    entryExtraBites: 120            //how many extra bites to add to the size of each entry to account for metadata
-}
---
+++ parameters.ts
@@ -0,0 +1,4 @@
+export const params = {
+    ledgSetsMaxRat: 0.2,  //the maximum size of a set in ledger relative to the size of cache.entries
+    entryExtraBites: 110  //how many extra bites to add to the size of each entry to account for metadata
+}
